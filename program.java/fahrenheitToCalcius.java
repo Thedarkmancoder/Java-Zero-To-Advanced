689d05290ae5fd8ce3dbd3ae6758e445401c0deb
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-07 23:32:41 +0530

    fahrenheit to calcius conversion

---
+++ program.java/fahrenheitToCalcius.java
@@ -0,0 +1,10 @@
+
+// fahrenheit to calcius convwrsion
+public class fahrenheitToCalcius {
+    public static void main(String[] args){
+        double fahrenheit = 45;
+        double calcius = (fahrenheit-32)*5/9;
+        System.out.println("calcius : " + calcius);
+    }
+}
+//output : calcius : 7.222222222222
