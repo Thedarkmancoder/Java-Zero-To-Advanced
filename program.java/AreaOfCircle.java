37182afd0e24200667bf12ffba6ba26c5eb30f37
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-07 23:50:58 +0530

    Add Area of Circle program

---
+++ program.java/AreaOfCircle.java
@@ -0,0 +1,11 @@
+
+// calculate the area of circle
+public class AreaOfCircle {
+    public static void main(String[] args){
+        double radius = 20.5;
+        double area = Math.PI*radius*radius;
+        System.out.println("the area of circle is : " + area);
+    }
+}
+
+//output : the area of circle is : 1320.2543126711105
