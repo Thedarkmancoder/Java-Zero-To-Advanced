e082667091763cfe8e8f9ea48d5e57a23a51183b
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-15 20:02:24 +0530

    Multiple Print Statement

---
+++ main_java/multiplePrintStatement.java
@@ -0,0 +1,9 @@
+package main_java;
+
+public class multiplePrintStatement {
+    public static void main(String[] args){
+        System.out.println(" Nitish ");
+        System.out.println(" Kumar ");
+        System.out.println(" Sharma ");
+    }
+}
