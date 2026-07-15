ec9a729ca4138976b58d837ad270cf5a862e28ad
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-15 20:14:24 +0530

    Concatenation Example

---
+++ main_java/concataionExample.java
@@ -0,0 +1,8 @@
+package main_java;
+
+public class concataionExample {
+    public static void main(String[] args){
+        String  name= "NITISH";
+        System.out.println(" Hello " + name + "!");
+    }
+}
