0eab63bf62b1ee2188a25eeb29705c289a8f4045
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-15 20:06:09 +0530

    Printing with Escape Sequences

---
+++ main_java/printingwithescapesequences.java
@@ -0,0 +1,8 @@
+package main_java;
+
+public class printingwithescapesequences {
+    public static void main(String[] args){
+        System.out.println(" Hello \t java"); //tab space
+        System.out.println(" HELLO \n JAVA"); // New line
+    }
+}
