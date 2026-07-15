509cfbc8f0c10aca37268322cfea890da5490d25
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-15 20:19:54 +0530

    print VS println

---
+++ main_java1/printVsPrintln.java
@@ -0,0 +1,9 @@
+package main_java1;
+
+public class printVsPrintln {
+public static void main(String[] args){
+    System.out.print(" This is ");
+    System.out.println(" on the same line ");
+    System.out.println("Now on a new line ");
+}
+}
