b7b9bb74b20ceca1deb47722e379b02fc19ae98e
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-15 20:11:19 +0530

    Basic Arithmatic in Print

---
+++ main_java/basicArithmaticPrint.java
@@ -0,0 +1,8 @@
+package main_java;
+
+public class basicArithmaticPrint {
+    public static void main(String[] args){
+        System.out.println(" sum : " + (5+3) );
+        System.out.println(" product :" + (5*3));
+    }
+}
