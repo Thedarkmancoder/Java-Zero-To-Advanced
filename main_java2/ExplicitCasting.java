cfcc029d51fd3c7caae545450f41d25df6dae5d7
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-16 11:52:03 +0530

    Explicit Casting ( Double To Int )

---
+++ main_java2/ExplicitCasting.java
@@ -0,0 +1,11 @@
+// Explicit Casting ( Double to int )
+package main_java2;
+
+public class ExplicitCasting {
+    public static void main(String[] args){
+        double myDouble = 50.0;
+        int myInt = (int)myDouble;
+        System.out.println(myInt);
+    }
+}
+// output 50
