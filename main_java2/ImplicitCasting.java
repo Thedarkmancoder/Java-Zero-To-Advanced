041c568bd9a6cfd39acc9e411d1fb1b97755829a
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-16 11:47:14 +0530

    Implicit Casting ( Int to Double )

---
+++ main_java2/ImplicitCasting.java
@@ -0,0 +1,12 @@
+// Implicit Casting (int to double)
+package main_java2;
+
+public class ImplicitCasting {
+    public static void main(String[] args){
+        int myInt = 50;
+        double myDouble = myInt;
+        System.out.println(myDouble );
+    }
+}
+
+//output 50.0
