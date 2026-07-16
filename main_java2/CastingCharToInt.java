01bd0658bee68c5851bd17c490300447ba76f845
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-16 11:56:21 +0530

    Casting Char To Int

---
+++ main_java2/CastingCharToInt.java
@@ -0,0 +1,10 @@
+package main_java2;
+
+public class CastingCharToInt {
+    public static void main(String[] args){
+        char letter = 'A';
+        int asciiValue = letter; //'A' is 65 in ASCII
+        System.out.println("ASCII OF A " + asciiValue);
+    }
+}
+// output 65
