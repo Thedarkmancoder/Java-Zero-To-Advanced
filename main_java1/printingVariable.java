676d62a65651b08cc9289e92456acc6ec93d474c
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-16 11:20:26 +0530

    Printing Variable

---
+++ main_java1/printingVariable.java
@@ -0,0 +1,10 @@
+package main_java1;
+
+public class printingVariable {
+    public static void main(String[] args){
+        int age =20;
+        String name = " NITISH ";
+        System.out.println(" NAME IS : " + name);
+        System.out.println(" AGE IS : " + age);
+    }
+}
