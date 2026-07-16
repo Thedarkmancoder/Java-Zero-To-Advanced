2e8cb992a650618ae205e736ae8df89bf8f3d533
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-16 11:32:34 +0530

    Concatenation In Print

---
+++ main_java1/concatenationInPrint.java
@@ -0,0 +1,10 @@
+package main_java1;
+
+public class concatenationInPrint {
+    public static void main(String[] args){
+        String greating = "HELLO";
+        String target =  "WORLD";
+        System.out.println(greating  + "," +target + "!");
+    }
+}
+// output HELLO,WORLD!
