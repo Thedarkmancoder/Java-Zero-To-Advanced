fcfbbe8233c58c4c39201a9ab8e92e11541a3a06
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-16 11:41:02 +0530

    Multiple Variable In One Line

---
+++ main_java1/multipVarbIn1Line.java
@@ -0,0 +1,12 @@
+package main_java1;
+
+public class multipVarbIn1Line {
+    public static void main(String[] args){
+        int age = 20;
+        String name = "NITISH";
+        double number = 99.9;
+        System.out.println("Age : " + age + " , Name : " + name + " , Number : " + number);
+
+    }
+}
+//output Age : 20 , Name : NITISH , Number : 99.9
