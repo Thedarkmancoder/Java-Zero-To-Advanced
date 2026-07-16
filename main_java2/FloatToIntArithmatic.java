4763a8c27230e4d5f642e4d66f50360b111692b2
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-16 12:08:27 +0530

    Float To Int Arithmatic

---
+++ main_java2/FloatToIntArithmatic.java
@@ -0,0 +1,10 @@
+package main_java2;
+
+public class FloatToIntArithmatic {
+    public static void main(String[] args){
+        float num = 5.99f;
+        int result = (int) num + 2;
+        System.out.println(" Result : " + result);
+    }
+}
+//output 7
