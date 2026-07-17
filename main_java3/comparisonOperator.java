ced1139b552332550b0c5ae114497d014f8205d4
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-17 23:42:18 +0530

    Comparison Operator

---
+++ main_java3/comparisonOperator.java
@@ -0,0 +1,10 @@
+public class comparisonOperator {
+    public static void main(String[] args){
+        int m = 5;
+        int n = 10;
+        System.out.println(m==n);
+        System.out.println(m!=n);
+        System.out.println(m<n);
+        System.out.println(m>n);
+    }
+}
