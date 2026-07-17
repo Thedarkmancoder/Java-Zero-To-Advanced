7f5eb3ba9a30c4f3f074c118ce7b07face39c917
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-17 23:36:38 +0530

    Arithmatic Operator

---
+++ main_java3/arithmaticOperator.java
@@ -0,0 +1,11 @@
+public class arithmaticOperator{
+    public static void main(String[] args){
+        int x = 50;
+        int y = 7;
+        System.out.println("Sum of Two Number Is : " + (x+y));
+        System.out.println("Subtract of Two Number Is : " + (x-y));
+        System.out.println("Multiple of Two Number Is : " + (x*y));
+        System.out.println("Divide of Two Number Is : " + (x/y));
+        System.out.println("Modulas/Remainder of Two Number Is : " + (x%y));
+    }
+}
