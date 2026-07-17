bde0af847f77f6b04bd13d3e3ed86fd61a7d0dd5
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-17 23:46:08 +0530

    Logical Operator

---
+++ main_java3/LogicalOperator.java
@@ -0,0 +1,9 @@
+public class LogicalOperator {
+ public static void main(String[] args){
+    boolean isSunny = true;
+    boolean isWarm = false;
+    System.out.println(isSunny && isWarm);
+    System.out.println(isSunny || isWarm);
+    System.out.println(!isSunny);
+ }   
+}
