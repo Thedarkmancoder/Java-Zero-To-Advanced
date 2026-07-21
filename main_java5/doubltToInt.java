864012c77db5e3867c64590f9aa23f2ba345821e
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-22 00:02:15 +0530

    Convert Double to int Using Casting

---
+++ main_java5/doubltToInt.java
@@ -0,0 +1,8 @@
+public class doubltToInt {
+    public static void main(String[] args){
+        double val = 9.99;
+        int result = (int) val;
+        System.out.println("Original : " + val);
+        System.out.println("Converted : " + result);
+    }
+}
