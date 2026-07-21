ff966a037df126fc9cedf061d774696a67ebda94
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-21 23:58:05 +0530

    Compare Two Numbers and Print the Greater

---
+++ main_java5/Comp2NoPrtGret.java
@@ -0,0 +1,13 @@
+public class Comp2NoPrtGret {
+    public static void main(String[] args){
+        int a = 15;
+        int b = 20;
+        if(a>b){
+            System.out.println("Greater : " + a);
+        }else if(b>a){
+            System.out.println("Greater : " + b);
+        }else{
+            System.out.println("Both Number is are equal ");
+        }
+    }
+}
