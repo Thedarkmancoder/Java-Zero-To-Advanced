37834f9a12eeabbaa64a191c8487e40b56c0fe48
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-22 00:05:47 +0530

    Calculate Simple Interest

---
+++ main_java5/calSimpleInterest.java
@@ -0,0 +1,9 @@
+public class calSimpleInterest {
+ public static void main(String[] args){
+    double principle = 1000;
+    double rate = 5.5;
+    int time = 2;
+    double interest = (principle*rate*time);
+    System.out.println("Simple Interest : " + interest);
+ }   
+}
