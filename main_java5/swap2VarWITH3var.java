eb982850e8a58c5c327c7c533fb2f18e96d6c645
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-21 23:53:01 +0530

    Swap Two Variable Without a Third Variable

---
+++ main_java5/swap2VarWITH3var.java
@@ -0,0 +1,11 @@
+public class swap2VarWITH3var {
+public static void main(String[] args){
+    int x = 5;
+    int y = 10;
+    x = x+y; //x becomes 15
+    y= x-y;//y becomes 5
+    x = x-y; // x becomes 10
+    System.out.println("x = " + x + " , y = " +y);
+}
+    
+}
