8a4b13545a5606e870df53abd855214182e97841
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-23 23:55:21 +0530

    Substring in String

---
+++ main_java6/Substring.java
@@ -0,0 +1,7 @@
+public class Substring {
+    public static void main(String[] args) {
+        String text = "Java Programing";
+        System.out.println(text.substring(4));
+        System.out.println(text.substring(0,4));
+    }
+}
