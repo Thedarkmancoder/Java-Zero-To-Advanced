d2969490245a88f021348d0663d3c32b3d4c2e39
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-23 23:58:00 +0530

    String Method

---
+++ main_java6/StringMethod.java
@@ -0,0 +1,8 @@
+public class StringMethod {
+    public static void main(String[] args) {
+        String pharse = "hello world ";
+        System.out.println(pharse.toUpperCase());
+        System.out.println(pharse.toLowerCase());
+        System.out.println(pharse.charAt(1));
+    }
+}
