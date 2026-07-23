dc5a787aceb565edfbd9a5564f634fe21f20a350
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-23 23:52:46 +0530

    Concatenation in String

---
+++ main_java6/concatation.java
@@ -0,0 +1,8 @@
+public class concatation {
+    public static void main(String[] args) {
+        String FirstName = "Nitish";
+        String LastName = "Sharma";
+        String FullName = FirstName + "" + LastName;
+        System.out.println(FullName);
+    }
+}
