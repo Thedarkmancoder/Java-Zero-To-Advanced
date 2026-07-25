1b2dd1be6bf2f5d8ab37c35a47e9e6577b90c139
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:28:48 +0530

    Boolean Variable

---
+++ main_java8/BooleanVariable.java
@@ -0,0 +1,8 @@
+package main_java8;
+
+public class BooleanVariable {
+    public static void main(String[] args) {
+        boolean isjavaFun = true;
+        System.out.println("isjavafun? " +isjavaFun);
+    }
+}
