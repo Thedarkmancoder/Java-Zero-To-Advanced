28681faf21529646fd83e3f754823e44b2e7b0d8
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:22:48 +0530

    Max And Min

---
+++ main_java7/MaxAndMin.java
@@ -0,0 +1,12 @@
+package main_java7;
+
+public class MaxAndMin {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Max : " + Math.max(a, b));
+        System.out.println("Min : " + Math.min(a, b));
+    }
+}
+// output : Max : 20 
+// Min : 10
