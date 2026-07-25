3b2f4aefe9e842f1296d34c27a5045931a221e77
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:45:53 +0530

    If Else Statement

---
+++ main_java9/ifElseStatement.java
@@ -0,0 +1,12 @@
+package main_java9;
+
+public class ifElseStatement {
+    public static void main(String[] args) {
+        int age = 16;
+        if(age>=18){
+            System.out.println("Adult : ");
+        }else{
+            System.out.println("Minor ");
+        }
+   }
+}
