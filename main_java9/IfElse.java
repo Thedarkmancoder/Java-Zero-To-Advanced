ddad8f2b9da0c820c0a37ef90771c8cf483ecb92
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:41:56 +0530

    Basic If Statement

---
+++ main_java9/IfElse.java
@@ -0,0 +1,10 @@
+package main_java9;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int x = 5;
+        if(x>0){
+            System.out.println(" X is Positive ");
+        }
+    }
+}
