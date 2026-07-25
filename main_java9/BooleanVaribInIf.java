5a098647a5bca4dd2d4845bce3b34d060615c0ea
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:55:49 +0530

    Boolean Variable In If

---
+++ main_java9/BooleanVaribInIf.java
@@ -0,0 +1,12 @@
+package main_java9;
+
+public class BooleanVaribInIf {
+    public static void main(String[] args) {
+        boolean isValid = false;
+        if(isValid){
+            System.out.println("Valid Input ");
+        }else{
+            System.out.println("Invalid Input ");
+        }
+    }
+}
