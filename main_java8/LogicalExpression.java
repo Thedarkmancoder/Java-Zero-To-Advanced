73abe5305164cbe82599069d50ba9b7cde3a55f0
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:37:51 +0530

    Logical Expression

---
+++ main_java8/LogicalExpression.java
@@ -0,0 +1,10 @@
+package main_java8;
+
+public class LogicalExpression {
+    public static void main(String[] args){
+    int age = 18;
+    boolean canVote = (age>=18);
+    System.out.println("Can Vote ? " + canVote);
+}
+}
+//output Can Vote ? true
