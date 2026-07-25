37df5cb31241d2b8d5608e7e7abe4e4a24b6d9da
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:51:00 +0530

    Else If Lodder

---
+++ main_java9/ElseIfLodder.java
@@ -0,0 +1,15 @@
+package main_java9;
+
+public class ElseIfLodder {
+    public static void main(String[] args) {
+        int marks = 85;
+        if(marks>=90){
+            System.out.println("Grade A ");
+        }else if(marks >= 80){
+            System.out.println("Grade B ");
+        }else{
+            System.out.println("Grade C ");
+        }
+    }
+}
+// output Grade B
