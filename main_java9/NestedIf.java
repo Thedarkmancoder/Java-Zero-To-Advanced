a64f74d3f40940999d4a14ecfda75c650f102259
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:53:03 +0530

    Nested If

---
+++ main_java9/NestedIf.java
@@ -0,0 +1,12 @@
+package main_java9;
+
+public class NestedIf {
+    public static void main(String[] args) {
+        int num = 10;
+        if(num>0){
+            if(num<20){
+                System.out.println("Boolean 1 and 19  ");
+            }
+        }
+    }
+}
