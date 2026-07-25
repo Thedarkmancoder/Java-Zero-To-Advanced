836d39b91fb619d747660a6846a44aa73e411a7d
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:34:26 +0530

    Using Boolean In If

---
+++ main_java8/UsingBoolInIf.java
@@ -0,0 +1,13 @@
+package main_java8;
+
+public class UsingBoolInIf {
+    public static void main(String[] args){
+        boolean isRaining = false;
+        if(isRaining){
+            System.out.println("Take an Umbrella ");
+        }else{
+            System.out.println("No Umbrella Needed ");
+        }
+    }
+}
+// output No Umbrella Needed
