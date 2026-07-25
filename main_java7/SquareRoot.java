77a2b84a487c6f768910b6ed7e843f2db1b735d8
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:17:42 +0530

    Square Root

---
+++ main_java7/SquareRoot.java
@@ -0,0 +1,8 @@
+package main_java7;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        double number = 16.0;
+        System.out.println(Math.sqrt(number));
+    }
+}
