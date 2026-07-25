93e9762036717fe7682e89c5855cb7e583c30950
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:39:52 +0530

    Negation

---
+++ main_java8/Negation.java
@@ -0,0 +1,9 @@
+package main_java8;
+
+public class Negation {
+    public static void main(String[] args) {
+        boolean success = true;
+        System.out.println("Not sucess ? " + success);
+    }
+}
+// output Not sucess ? true
