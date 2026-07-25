8b21fcc8c93872e46f85acfb84141d229452006a
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:31:22 +0530

    Boolean From Comparison

---
+++ main_java8/BoolFromComp.java
@@ -0,0 +1,11 @@
+package main_java8;
+
+public class BoolFromComp {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        boolean result = x>y;
+        System.out.println("Is x>y? " + result);
+    }
+}
+//output : Is x>y? false
