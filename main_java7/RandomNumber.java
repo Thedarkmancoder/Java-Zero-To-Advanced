1824bf3ea89096505a064308e9a01938c02895f2
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-25 23:26:04 +0530

    Random Number

---
+++ main_java7/RandomNumber.java
@@ -0,0 +1,11 @@
+package main_java7;
+
+public class RandomNumber {
+    public static void main(String[] args){
+        double randomNumber = Math.random();
+        // 0.0<= randomValue < 1.0
+        System.out.println("Random : " + randomNumber);
+    }
+}
+
+// output o.447426122.......
