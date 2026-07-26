16a835125e6d2797fb6496eeb215eb52adda4a07
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-26 13:14:00 +0530

    Switch With Fall-Through

---
+++ main_java10/SwtWthFalThrogh.java
@@ -0,0 +1,18 @@
+package main_java10;
+
+public class SwtWthFalThrogh {
+public static void main(String[] args){
+    int month = 4   ;
+    switch(month){
+        case 3 :
+            case 4:
+                case 5 :
+                    System.out.println("Spring Season ");
+                    break;
+                    default :
+                    System.out.println(" Not Spring ");
+    }
+}    
+
+}
+// putput Spring seaseon
