d263499c75fd00183a73727beee272979107dc57
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-26 13:32:46 +0530

    Basic While

---
+++ main_java11/basicWhile.java
@@ -0,0 +1,17 @@
+package main_java11;
+
+public class basicWhile {
+    public static void main(String[] args){
+        int i = 0;
+        while(i<5){
+            System.out.println("i = " + i);
+            i++;
+        } 
+    }
+}
+// output 
+// i = 0
+// i = 1
+// i = 2
+// i = 3
+// i = 4
