3cd2de8622793c07ae6169bb5d9d36bc8dcefc33
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-26 13:36:20 +0530

    Counting Down

---
+++ main_java11/CountingDown.java
@@ -0,0 +1,17 @@
+package main_java11;
+
+public class CountingDown {
+    public static void main(String[] args){
+        int j = 5;
+        while(j>0){
+            System.out.println(j);
+            j--;
+        }
+    }
+}
+// output 
+// 5
+// 4
+// 3
+// 2
+// 1
