61615946ca37fa05afb831a541edbb84ca2ba398
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-27 12:59:39 +0530

    For-Each Loop (Arrays)

---
+++ main_java12/ForEachLoop.java
@@ -0,0 +1,15 @@
+package main_java12;
+
+public class ForEachLoop {
+    public static void main(String[] args){
+        int[] nums = {1,3,4,5};
+        for(int n : nums){
+            System.out.println(n);
+        }
+    }
+}
+// output
+// 1
+// 3
+// 4
+// 5
