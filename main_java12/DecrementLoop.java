510a717c6b7cbaab44982ab450029e8162e61010
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-27 13:09:08 +0530

    Decrement Loop

---
+++ main_java12/DecrementLoop.java
@@ -0,0 +1,15 @@
+package main_java12;
+
+public class DecrementLoop {
+    public static void main(String[] args) {
+        for(int i = 5; i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
+// output
+// 5
+// 4
+// 3
+// 2
+// 1
