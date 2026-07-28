8e2015f0a9c65004adf933626723d35c96213e0d
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-28 20:19:19 +0530

    For-Each Loop

---
+++ main_java14/ArrayForEach.java
@@ -0,0 +1,12 @@
+public class ArrayForEach {
+    public static void main(String[] args){
+        String[] fruits = {"Apple", "Orange", "Banana"};
+        for(String fruit : fruits){
+            System.out.println(fruit);
+        }
+    }
+}
+// output 
+// Apple
+// Orange
+// Banana
