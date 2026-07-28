9002f60f396d3d8de646ae64120297287f25d944
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-28 20:32:04 +0530

    Basic Method Declaration

---
+++ main_java15/BasicMethodExample.java
@@ -0,0 +1,13 @@
+package main_java15;
+
+public class BasicMethodExample {
+    public void greet(){
+        System.out.println("Hello Nitish ");
+    }
+public static void main(String[] args){
+    BasicMethodExample example = new BasicMethodExample();
+    example.greet();
+}
+}
+// output
+// Hello Nitish
