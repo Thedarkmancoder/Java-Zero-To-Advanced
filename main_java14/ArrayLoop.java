d34f740715014e5cba60e62a3c1768c266507aa2
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-28 20:15:25 +0530

    Iterate Using For Loop

---
+++ main_java14/ArrayLoop.java
@@ -0,0 +1,18 @@
+public class ArrayLoop {
+    public static void main(String[] args){
+        int[] nums = {1,2,3,4,5,6,7,8};
+        for(int i=0;i<nums.length; i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
+
+// output
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
