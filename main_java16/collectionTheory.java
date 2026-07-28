880cd72e876fca610c6d82d3b0085cf63117154d
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-29 01:38:30 +0530

    Java Collection

---
+++ main_java16/collectionTheory.java
@@ -0,0 +1,43 @@
+package main_java16;
+
+//import java.util.ArrayList;
+
+public class collectionTheory {
+    public static void main(String[] args){
+        System.out.println(" Introduction To Collection ");
+        System.out.println("Definition :-\r\n" + //
+                        "collections are data structure that groups multiple elements into a single unit.\r\n" + //
+                        "The java collections framwork(in java.utile) provides standard interfaces (list, Set, Map)\r\n" + //
+                        "and their implementations.\r\n" + //
+                        "\r\n" + //
+                        "Advantages Over Arrays:-\r\n" + //
+                        "Dynamic Sizing:- automatically adjusts to the number of elements. \r\n" + //
+                        "\r\n" + //
+                        "RICH API:- method for adding, removing, searching,sorting etc . \r\n" + //
+                        "\r\n" + //
+                        "Type Safety:- use of generics minimizes runtime type errors. \r\n" + //
+                        "\r\n" + //
+                        "Utility:- built in support for common algorithms(eg . sorting). \r\n" + //
+                        "\r\n" + //
+                        "Use Cases:-\r\n" + //
+                        "managing Dynamic user inputs(shopping carts, to-do lists). \r\n" + //
+                        "data-driven applications with frequently abanging data sets. \r\n" + //
+                        "\r\n" + //
+                        "What is ArrayList:-\r\n" + //
+                        "A resizable-array implementations of the List interfaces. \r\n" + //
+                        "maintain insertion order and allows duplicates elements. \r\n" + //
+                        "\r\n" + //
+                        "Core methods:-\r\n" + //
+                        "Addition:- add(element) and add(index , element). \r\n" + //
+                        "Access:- get(index). \r\n" + //
+                        "Update:- Set(index , element). \r\n" + //
+                        "Removal:- remove(index) or remove(object).\r\n" + //
+                        "other methods:- size(), contains(element), clear(), indexof(element). \r\n" + //
+                        "\r\n" + //
+                        "Featues:- \r\n" + //
+                        "Random access with index lookup. \r\n" + //
+                        "Ideal for Dynamic, frequently modified data. ");
+
+
+    }
+}
