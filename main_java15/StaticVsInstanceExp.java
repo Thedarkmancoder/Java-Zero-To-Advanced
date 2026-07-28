2f34bfb820c7ca07e11fcc1a6438651f7b84c718
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-28 20:54:30 +0530

    modify code and Solve Error

--- main_java15/StaticVsInstanceExp.java
+++ main_java15/StaticVsInstanceExp.java
@@ -4,7 +4,7 @@ public class StaticVsInstanceExp {
     public static int multiply(int a , int b){
         return a*b;
     }
-    public static int subtract(int a , int b){
+    public  int subtract(int a , int  b){
         return a-b;
     }
     public static void main(String[] args){
