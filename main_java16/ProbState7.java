8dfc2882e9527bbee2e453983bff72d0ef40edf1
Nitish Kumar <thedarkmancoder@gmail.com>
2026-07-30 01:46:20 +0530

    Checking For an Element

--- main_java16/ProbState7.java
+++ main_java16/ProbState7.java
@@ -1,3 +1,21 @@
+// Problem Statement:- Create an ArrayList of strings and check if it contains "Bob". 
+// Hint:- Use contains() to verify if the list includes the given element.
+
+import java.util.ArrayList;
 public class ProbState7 {
-    
+    public static void main(String[] args){
+     ArrayList<String>names = new ArrayList<>();
+        names.add("Nitish");
+        names.add("Chandan");
+        names.add("Nilesh");
+        names.add("Aniket");
+        names.add("Bob");
+        names.add("Kalu");
+        names.add("Abhishek");
+        names.add("Sanat");
+        boolean Found = names.contains("Bob");
+        System.out.println("Contains 'Bob' ? " + Found);   
+    }
 }
+// output 
+// Contains 'Bob' ? true
