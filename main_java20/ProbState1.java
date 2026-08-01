dd5322de54ff7f28852a5f2aa584f4bedab1cf48
Nitish Kumar <thedarkmancoder@gmail.com>
2026-08-01 23:33:05 +0530

    Creating and Populating a HashMap

---
+++ main_java20/ProbState1.java
@@ -0,0 +1,17 @@
+// Problem Statement:- Create a  HashMap of Country-capital Pairs and print the Map.
+// Hint:- Use put(key , value) to add entries.
+
+import java.util.HashMap;
+public class ProbState1{
+ public static void main(String[] args){
+    HashMap<String, String>capitals = new HashMap<>();
+    capitals.put("India", "Delhi");
+    capitals.put("Japan","Tokyo");
+    capitals.put("Germany", "Berlin");
+    capitals.put("Canada", "Ottawa");
+    capitals.put("France", "Paris");
+    capitals.put("Brazil", "Brasilia");
+    capitals.put("USA" , "Washington , D.C");
+    System.out.println(" Country Capitals : " + capitals);
+ }   
+}
